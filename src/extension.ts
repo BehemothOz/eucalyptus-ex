1c2a62d1d0168b568c4e51bae4b7a790d02c8157
behemothoz <behemothoz@yandex.ru>
2024-05-05 18:27:34 +0300

    check commit

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('stencil-ui.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from stencil-ui!');
+		vscode.window.showInformationMessage('Hello World');
 	});
 
 	context.subscriptions.push(disposable);
