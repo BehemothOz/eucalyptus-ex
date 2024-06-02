a95502ec62622b3f1866d97493d836433aaaec6d
behemothoz <behemothoz@yandex.ru>
2024-06-02 14:01:26 +0300

    jsDoc for template

--- src/stencil/core/templates/Templates.ts
+++ src/stencil/core/templates/Templates.ts
@@ -1,16 +1,38 @@
+/**
+ * Enum for template keys.
+ * @readonly
+ * @enum {string}
+ */
 export enum TemplateKey {
     DIAMOND = 'diamond_template',
     RUBY = 'ruby_template',
     SAPPHIRE = 'sapphire_template',
 }
 
+/**
+ * Interface for a template manager.
+ * @interface
+ */
 export interface Template {
+    /**
+     * The key of the template. A unique property.
+     */
     key: TemplateKey;
+    /**
+     * The label of the template. Used for UI display.
+     */
     label: string;
+    /**
+     * The detail description of the template. Used for UI display.
+     */
     detail: string;
 }
 
 export interface ITemplatesManager {
+    /**
+     * Gets an array of templates.
+     * @returns {Template[]} An array of Template objects.
+     */
     getTemplates: () => Array<Template>;
 }
 
@@ -32,6 +54,10 @@ const templates: Array<Template> = [
     },
 ];
 
+/**
+ * Class representing a templates manager.
+ * @implements {ITemplatesManager}
+ */
 export class TemplatesManager implements ITemplatesManager {
     private templates = templates;
 
